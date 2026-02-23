1737a6e8e59ebf2e9a15aeb67afe48cc01c50589
Fernando Sobron <llamamealpodcast@gmail.com>
2026-02-23 08:36:41 +0000

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -8,18 +8,18 @@
   </v-app>
 </template>
 
-export default { head() { return { title: 'Fernando Sobron | Creative Producer,
-Director & Illustrator', meta: [ { hid: 'description', name: 'description',
-content: 'Portfolio of Fernando Sobron PhD, an award-winning Advertising
-Creative, Producer, Director, and Illustrator specializing in unscripted TV and
-documentaries at Barn Studio, Heathfield, East Sussex, UK.' }, { name:
-'keywords', content: 'Fernando Sobron, Producer, Unscripted TV Director,
-Documentary Filmmaker, Illustrator, Advertising Creative, Nuxt Portfolio' }, //
-Social Media (Open Graph) { property: 'og:title', content: 'Fernando Sobron |
-Creative Producer & Illustrator' }, { property: 'og:description', content:
-'Explore the work of Fernando Sobron at Barn Studio.' }, { property: 'og:image',
-content: '/path-to-your-og-image.jpg' } // Link to a nice screenshot of your
-site ] } } }
+export default { head() { return { title: 'Fernando Sobron | Barn Studio -
+Producer Director & Illustrator', meta: [ { hid: 'description', name:
+'description', content: 'Portfolio of Fernando Sobron PhD at Barn Studio.
+Award-winning Advertising Creative, Producer Director, and Illustrator
+specializing in unscripted TV and documentaries, Barn Studio, Heathfield, East
+Sussex' }, { name: 'keywords', content: 'Fernando Sobron, Producer Director,
+Unscripted TV, Producer, Podcasts, Documentary, Illustration, Barn Studio,
+Heathfield, East Sussex' }, // Social Media (Open Graph) { property: 'og:title',
+content: 'Fernando Sobron | Creative Producer & Illustrator' }, { property:
+'og:description', content: 'Explore the work of Fernando Sobron PhD at Barn
+Studio.' }, { property: 'og:image', content: '/path-to-your-og-image.jpg' } //
+Link to a nice screenshot of your site ] } } }
 
 <script>
 import PortfolioMain from '@/components/Main'
